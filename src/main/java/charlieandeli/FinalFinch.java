08e8815ba165a55bcea0edbf15d1699c070f4bd3
vivacephoenix <charlie.p.auclair@gmail.com>
2026-05-28 12:43:27 -0700

    init commit
    init commit

---
+++ src/main/java/charlieandeli/FinalFinch.java
@@ -0,0 +1,9 @@
+package com.charlieandeli
+
+import com.birdbrain.Finch;
+
+public class FinalFinch extends Finch {
+    public FinalFinch(String device) {
+        super(device);
+    }
+}
---
+++ src/main/java/charlieandeli/TesterFinch.java
@@ -0,0 +1,10 @@
+package com.charlieandeli
+
+public class TesterFinch {
+
+public static void main(String[] args) {
+    FinalFinch bird = new FinalFinch("A")
+    bird.setMove("F", 10, 50);
+    }
+
+}
